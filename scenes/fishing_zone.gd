5fdc29482507361891b94ffb46c96c21e88aecf0
IbraFar <ibrahimfarhane@gmail.com>
2024-12-15 20:49:12 -0500

    Temp fix for animation bug with fishing prompt

---
+++ scenes/fishing_zone.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+var player_in_range = false
+@onready var fishing_button = $FishingButton
+@onready var animation_player = $ButtonPress
+
+func _ready():
+	fishing_button.hide()  # Hide button initially
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_range = true
+		fishing_button.show()
+		animation_player.play("fishing_button_press")
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_range = false
+		fishing_button.hide()
+		animation_player.stop()  # Stop the animation when player leaves
+
+func _process(_delta: float) -> void:
+	if player_in_range and Input.is_action_just_pressed("fish"):
+		start_fishing()
+
+func start_fishing():
+	get_tree().change_scene_to_file("res://scenes/FishingMinigame.tscn")
